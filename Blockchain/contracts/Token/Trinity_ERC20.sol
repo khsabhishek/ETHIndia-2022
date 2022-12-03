6f709b3224576381fccfb766d7f12a1dd3139202
Ayush Ranjan <ayusranjan@gmail.com>
2022-12-03 22:42:36 +0530

    added address whitelist

--- Blockchain/contracts/Token/Trinity_ERC20.sol
+++ Blockchain/contracts/Token/Trinity_ERC20.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@4.8.0/security/Pausable.sol";
 import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
 
-contract Test is ERC20, ERC20Burnable, Pausable, Ownable {
-    constructor() ERC20("Trinity", "TR") {}
+contract TrinityStablecoin is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Trinity", "TR") {
+        whitelist[msg.sender] = true;
+    }
+
+    mapping(address => bool) public whitelist;
+
+    function addToWhitelist(address _whitelistAddress) external onlyOwner {
+        whitelist[_whitelistAddress] = true;
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -17,7 +25,7 @@ contract Test is ERC20, ERC20Burnable, Pausable, Ownable {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 
@@ -27,5 +35,10 @@ contract Test is ERC20, ERC20Burnable, Pausable, Ownable {
         override
     {
         super._beforeTokenTransfer(from, to, amount);
+    }
+    
+    modifier onlyWhitelist() {
+        require(whitelist[msg.sender] == true, "Caller is not whitelisted to call this function");
+        _;
     }
 }
